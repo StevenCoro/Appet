1af2d877ffc735e466d13daed1aff877d7ebc4e7
StevenCoro <steven-coro@hotmail.com>
2018-05-03 21:51:17 -0500

    Registro en txt

--- app/src/main/java/web/appet/InicioSesion.java
+++ app/src/main/java/web/appet/InicioSesion.java
@@ -1,5 +1,4 @@
 package web.appet;
-
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -8,42 +7,36 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
-import android.widget.Toast;
-
 public class InicioSesion extends AppCompatActivity {
-    private EditText correoInicioSesion, contraseñaInicioSesion;
+    private EditText etCorreoInicioSesion, etContraseñaInicioSesion;
     private CheckBox checkBoxInicioSesion;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio_sesion);
 
-        correoInicioSesion=(EditText)findViewById(R.id.correoInicioSesion);
-        contraseñaInicioSesion=(EditText)findViewById(R.id.contraseñaInicioSesion);
-        checkBoxInicioSesion =(CheckBox)findViewById(R.id.checkBoxInicioSesion);
+        etCorreoInicioSesion = (EditText) findViewById(R.id.etCorreoInicioSesion);
+        etContraseñaInicioSesion = (EditText) findViewById(R.id.etContraseñaInicioSesion);
+        checkBoxInicioSesion = (CheckBox) findViewById(R.id.checkBoxInicioSesion);
         SharedPreferences verificarPreferences = getSharedPreferences("datos", Context.MODE_PRIVATE);
-        contraseñaInicioSesion.setText(verificarPreferences.getString("contraseña", ""));
-        correoInicioSesion.setText(verificarPreferences.getString("correo", ""));
-
-
-
+        etContraseñaInicioSesion.setText(verificarPreferences.getString("contraseña", ""));
+        etCorreoInicioSesion.setText(verificarPreferences.getString("correo", ""));
     }
-    public void Registrarse(View view){
 
+    public void Registrarse(View view) {
         Intent i = new Intent(this, PantallaRegistro.class);
-        startActivity(i);finish();
-}
-    public void IniciarSesion(View view){
-        if(checkBoxInicioSesion.isChecked()==true){
-        SharedPreferences crearPreferences = getSharedPreferences("datos", Context.MODE_PRIVATE);
-        SharedPreferences.Editor editarPreferences = crearPreferences.edit();
-        editarPreferences.putString("correo", correoInicioSesion.getText().toString());
-        editarPreferences.putString("contraseña", contraseñaInicioSesion.getText().toString());
-        editarPreferences.commit(); finish();}
-
+        startActivity(i);
+        finish();
     }
 
-
-
-
+    public void IniciarSesion(View view) {
+        if (checkBoxInicioSesion.isChecked() == true) {
+            SharedPreferences crearPreferences = getSharedPreferences("datos", Context.MODE_PRIVATE);
+            SharedPreferences.Editor editarPreferences = crearPreferences.edit();
+            editarPreferences.putString("correo", etCorreoInicioSesion.getText().toString());
+            editarPreferences.putString("contraseña", etContraseñaInicioSesion.getText().toString());
+            editarPreferences.commit();
+            finish();
+        }
+    }
 }
--- app/src/main/java/web/appet/PantallaRegistro.java
+++ app/src/main/java/web/appet/PantallaRegistro.java
@@ -1,27 +1,61 @@
 package web.appet;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
 public class PantallaRegistro extends AppCompatActivity {
-    private EditText etCorreo, etNombre, etContraseña, etContraseña2;
+    private EditText etCorreo, etNombre, etContraseña, etContraseña2, correoInicioSesion;
+    private RadioButton rbInicioSesion;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantalla_registro);
         etCorreo = (EditText)findViewById(R.id.correoRegistro);
+        correoInicioSesion =(EditText)findViewById(R.id.etCorreoInicioSesion);
         etNombre = (EditText)findViewById(R.id.nombreRegistro);
         etContraseña= (EditText)findViewById(R.id.contraseñaRegistro);
         etContraseña2 = (EditText)findViewById(R.id.contraseñaRegistro2);
+        rbInicioSesion = (RadioButton)findViewById(R.id.radioButton);
+        String archivos[] = fileList();
+        if (ArchivoExiste(archivos, "datos.txt")){
+            try {
+                InputStreamReader archivo = new InputStreamReader(openFileInput("datos.txt"));
+                BufferedReader leerArchivo = new BufferedReader(archivo);
+                String guardar = leerArchivo.readLine();
+                String guardarTodoTexto = "";
+
+                while(guardar!=null){
+                    guardarTodoTexto= guardarTodoTexto + guardar + "/n";
+                    guardar=leerArchivo.readLine();
+                }
+                leerArchivo.close();
+                archivo.close();
+
+
+            }catch (IOException e){}
+        }
 
 
 
-
+    }private boolean ArchivoExiste(String archivos[], String NombreArchivo){
+     for (int i=0; i<archivos.length; i++)
+         if(NombreArchivo.equals(archivos[i]))
+             return true;
+     return false;
     }
+
+
 
     public void Registrar (View view) {
         String correo = etCorreo.getText().toString();
@@ -29,35 +63,38 @@ public class PantallaRegistro extends AppCompatActivity {
         String contraseña = etContraseña.getText().toString();
         String contraseña2 = etContraseña2.getText().toString();
 
-        if (correo.length()==0){
-            Toast.makeText(this, "Debes ingresar un correo", Toast.LENGTH_LONG).show();
-        }
 
-        if (nombre.length() == 0) {
-            Toast.makeText(this, "Debes ingresar un nombre", Toast.LENGTH_LONG).show();
-        }
-        if (contraseña.length() == 0) {
-            Toast.makeText(this, "Ingresa una contraseña", Toast.LENGTH_LONG).show();
-        }
-        if (contraseña2.length()==0) {
-            Toast.makeText(this, "Confirma tu contraseña", Toast.LENGTH_LONG).show();
-            if (contraseña!=contraseña2&&contraseña2.length()!=0) {
-                Toast.makeText(this, "Las contraseñas deben coincidir", Toast.LENGTH_LONG).show();
+        if (rbInicioSesion.isChecked() == true) {
+            if (correo.length() == 0) {
+                Toast.makeText(this, "Debes ingresar un correo", Toast.LENGTH_SHORT).show();
+            } else if (nombre.length() == 0) {
+                Toast.makeText(this, "Debes ingresar un nombre", Toast.LENGTH_SHORT).show();
+            } else if (contraseña.length() == 0) {
+                Toast.makeText(this, "Ingresa una contraseña", Toast.LENGTH_SHORT).show();
+            } else if (contraseña2.length() == 0 && contraseña.length() != 0) {
+                Toast.makeText(this, "Confirma tu contraseña", Toast.LENGTH_SHORT).show();
             }
+            if (contraseña.length() != 0 && contraseña2.length() != 0 && contraseña != contraseña2) {
+                Toast.makeText(this, "Las contraseñas deben coincidir", Toast.LENGTH_SHORT).show();
+            }
+            if (nombre.length() != 0 && contraseña.length() != 0 && contraseña.equals(contraseña2) && (correo.length() != 0)) {
+                Toast.makeText(this, "Se ha registrado", Toast.LENGTH_SHORT).show();
+                Intent i = new Intent(this, InicioSesion.class);
+                startActivity(i);
+                try {
+                    OutputStreamWriter enviarArchivo = new OutputStreamWriter(openFileOutput("datos.txt", Activity.MODE_PRIVATE));
+                    enviarArchivo.write(etCorreo.getText().toString());
+                    enviarArchivo.flush();
+                    enviarArchivo.close();
+                }catch (IOException e){}
+                finish();
+            }
+        } else if (rbInicioSesion.isChecked() == false) {
+            Toast.makeText(this, "Debes aceptar términos y condiciones", Toast.LENGTH_SHORT).show();
         }
-
-        else if(contraseña.equals(contraseña2) && (correo.length()!=0)){
-            Toast.makeText(this, "Se ha registrado", Toast.LENGTH_LONG).show();
-            Intent i = new Intent(this, Menu.class);startActivity(i);finish();
-
-
-
-
-        }
-
-
-
-
-
+    }
+    public void TerminosCondiciones(View view){
+        Intent i = new Intent(this, Menu.class);
+        startActivity(i);finish();
     }
 }
